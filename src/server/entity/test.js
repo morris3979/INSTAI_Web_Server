034ab173fea6c69824868261e7434242402519e3
Enchi <enchi0215@gmail.com>
2021-12-23 15:19:14 +0800

    add typeorm

---
+++ src/server/entity/test.js
@@ -0,0 +1,16 @@
+var EntitySchema = require("typeorm").EntitySchema;
+
+module.exports = new EntitySchema({
+    name: "Test", // Will use table name `Test` as default behaviour.
+    tableName: "test", // Optional: Provide `tableName` property to override the default behaviour for table name.
+    columns: {
+        id: {
+            primary: true,
+            type: "int",
+            generated: true
+        },
+        name: {
+            type: "varchar"
+        }
+    }
+});
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const os = require('os');
+const { createConnection } = require('typeorm');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+createConnection(); //typeorm connection
+
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+// app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
