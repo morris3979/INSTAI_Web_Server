4e4a91c3badf5cdbaa8dd9c6a1f73fcbf1c86e7d
EnChi <enchi0215@gmail.com>
2021-12-27 17:06:19 +0800

    update api

--- src/server/routes/carNumber.js
+++ src/server/routes/carNumber.js
@@ -1,9 +1,32 @@
 const { Request, Router } = require('express');
+const { getRepository } = require('typeorm');
 const carnumberRouter = Router({});
 
+const {CarNumber} = require('../entity/database');
 
-carnumberRouter.get('/', function(req, res) {
-  res.send('hello world');
+
+carnumberRouter.get('/', async(req, res) => {
+  try{
+    let carNumber = (
+      await getRepository(CarNumber).find().map()
+    )
+    res.json(carNumber);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 });
+
+carnumberRouter.post("/", async (req, res) => {
+  try{
+    const {boardId, modelName} = req.body;
+    const carNumber = new CarNumber({boardId, modelName});
+    const ret = await carNumber.save();
+    res.json(ret);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+})
 
 module.exports = carnumberRouter;
