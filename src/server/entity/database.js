a2065b38e89270adc0f4b074c485f574839dfcdb
EnChi <enchi0215@gmail.com>
2021-12-27 14:49:32 +0800

    fix timeout

--- src/server/entity/database.js
+++ src/server/entity/database.js
@@ -2,7 +2,7 @@ const EntitySchema = require("typeorm").EntitySchema;
 
 module.exports = new EntitySchema({
     name: "Test", // Will use table name `Test` as default behaviour.
-    tableName: "test", // Optional: Provide `tableName` property to override the default behaviour for table name.
+    tableName: "carNunber", // Optional: Provide `tableName` property to override the default behaviour for table name.
     columns: {
         id: {
             primary: true,
