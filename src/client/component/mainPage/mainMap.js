7aac052ba8cd55da3af1e56820a921e38a966c8b
shihmenghao <a0970047551@gmail.com>
2022-01-03 21:41:51 +0800

    JSX編排調整

--- src/client/component/mainPage/mainMap.js
+++ src/client/component/mainPage/mainMap.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import 'leaflet/dist/leaflet.css'
 import { MapContainer, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
 
 const MainMap = () => {
   return (
-    <MapContainer center={[25.0426, 121.537]} zoom={17} style={{ height: "100vh", width: "100vw" }}>
+    <MapContainer
+      center={[25.0426, 121.537]}
+      zoom={17}
+      style={{ height: "100vh", width: "100vw" }}
+    >
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
     </MapContainer>
   )
