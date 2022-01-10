4708b9b7bce1bcedf8464496d3ede949115aa181
EnChi <enchi0215@gmail.com>
2022-01-10 15:57:49 +0800

    update routes

--- src/server/routes/carnumberRouter.js
+++ src/server/routes/carnumberRouter.js
@@ -1,12 +1,21 @@
 const express = require('express');
-const { createConnection, getRepository } = require('typeorm');
 const carnumberRouter = express.Router();
-const { CarNumber } = require('../entity/CarNumber');
-const { Details } = require("../entity/Details")
+const { getCarNumbers, insertCarNumber } = require('../entity/CarNumber');
+const { Details } = require("../entity/Details");
+
+carnumberRouter.get('/test', async(req, res) => {
+  try{
+    res.send("TEST");
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
 
 carnumberRouter.get('/', async(req, res) => {
   try{
-    res.json(await getRepository(CarNumber).find());
+    const carnumbers = await getCarNumbers();
+    res.json(carnumbers);
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
@@ -14,15 +23,9 @@ carnumberRouter.get('/', async(req, res) => {
 });
 
 carnumberRouter.post("/upload", async (req, res) => {
-  const body = { boardId: string, modelName: string, version: string, driverlicense:string } = req.body;
-  // const newItem = new CarNumber();
-  // newItem.boardId = body.boardId || "";
-  // newItem.modelName = body.modelName || "";
-  // newItem.version = body.version || "";
-  // newItem.driverlicense = body || "";
   try{
-    await getRepository(CarNumber).save(body);
-    res.status(200).json();
+    const carnumbers = await insertCarNumber();
+    res.status(200).json(carnumbers);
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
