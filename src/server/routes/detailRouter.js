92d0cce80748ee6ae2f99879ba0eb2a5cad1ab16
EnChi <enchi0215@gmail.com>
2022-01-13 11:59:27 +0800

    add detailRouter

---
+++ src/server/routes/detailRouter.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const detailsRouter = express.Router();
+const db = require('../entity/db');
+
+detailsRouter.get('/', async(req, res) => {
+  try{
+    const details = await db.getDetails();
+    res.json(details);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+detailsRouter.post("/", async (req, res) => {
+  try{
+    const details = await db.insertCarNumber();
+    res.status(200).json(details);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+})
+
+module.exports = [ detailsRouter ];
