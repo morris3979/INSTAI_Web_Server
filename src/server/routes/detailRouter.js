002a6c356aeefee41e09169311f30288ea3de027
Enchi <enchi0215@gmail.com>
2022-02-17 23:02:21 +0800

    fix details api

--- src/server/routes/detailRouter.js
+++ src/server/routes/detailRouter.js
@@ -3,38 +3,17 @@ const detailsRouter = express.Router();
 const { getConnection } = require("../entity/db_config");
 const { Details } = require("../entity/db_constructor");
 
-async function getDetails() {
-  const connection = await getConnection();
-  const detailRepo = connection.getRepository(Details);
-  const details = await detailRepo.find({
-    relations: ["CarNumber"],
-  });
-  connection.close();
-  return details;
-}
-
-async function insertDetail(id, startingTime, gpsState, speed, event, position) {
-  const connection = await getConnection();
-  //create
-  const details = new Details();
-  details.id = id;
-  details.startingTime = startingTime;
-  details.gpsState = gpsState;
-  details.speed = speed;
-  details.event = event;
-  details.position = position;
-  //save
-  const detailRepo = connection.getRepository(Details);
-  const res = await detailRepo.save(details);
-  console.log("save", res);
-  //return new list
-  const allDetails = await detailRepo.find();
-  connection.close();
-  return allDetails;
-}
-
 //GET
 detailsRouter.get('/', async(req, res) => {
+  async function getDetails() {
+    const connection = await getConnection();
+    const detailRepo = connection.getRepository(Details);
+    const details = await detailRepo.find({
+      relations: ["CarNumber"],
+    });
+    connection.close();
+    return details;
+  }
   try{
     const details = await getDetails();
     res.json(details);
@@ -46,8 +25,34 @@ detailsRouter.get('/', async(req, res) => {
 
 //POST
 detailsRouter.post("/", async (req, res) => {
+  const startingTime = req.body.startingTime;
+  const gpsState = req.body.gpsState;
+  const speed = req.body.speed;
+  const event = req.body.event;
+  const position = req.body.position;
+  const createAt = new Date(new Date().toLocaleDateString());
+  async function insertDetail(startingTime, gpsState, speed, event, position, createAt) {
+    const connection = await getConnection();
+    //create
+    const details = new Details();
+    details.id = id;
+    details.startingTime = startingTime;
+    details.gpsState = gpsState;
+    details.speed = speed;
+    details.event = event;
+    details.position = position;
+    //save
+    const detailRepo = connection.getRepository(Details);
+    const res = await detailRepo.save(details);
+    console.log("save", res);
+    //return new list
+    const allDetails = await detailRepo.find();
+    await connection.getRepository(CarNumber).save(allDetails);
+    connection.close();
+    return allDetails;
+  }
   try{
-    const details = await insertDetail();
+    const details = await insertDetail(startingTime, gpsState, speed, event, position, createAt);
     res.status(200).json(details);
   } catch (e) {
     console.log(e);
