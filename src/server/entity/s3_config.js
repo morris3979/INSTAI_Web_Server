aefc97770d6888942295285667b4eb4003e39986
Enchi <enchi0215@gmail.com>
2022-03-05 13:23:41 +0800

    fix eslint

--- src/server/entity/s3_config.js
+++ src/server/entity/s3_config.js
@@ -1,19 +1,19 @@
 const AWS = require('aws-sdk');
 require('dotenv').config();
- 
+
 const s3Client = new AWS.S3({
 	region: process.env.AWS_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
- 
+
 const downloadParams = {
-    Bucket: process.env.AWS_BUCKET, 
+    Bucket: process.env.AWS_BUCKET,
     Key: '', // pass key
 };
 
 const s3 = {};
 s3.s3Client = s3Client;
 s3.downloadParams = downloadParams;
- 
+
 module.exports = s3;
