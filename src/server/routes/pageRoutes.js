de48b8302a121162dd24798ce537a6d803211c31
Enchi <enchi0215@gmail.com>
2022-05-30 14:49:30 +0800

    fix DJ/4196U,4

--- src/server/routes/pageRoutes.js
+++ src/server/routes/pageRoutes.js
@@ -4,31 +4,31 @@ const express = require('express');
 const pageRouter = express.Router();
 
 pageRouter.get("/map", async (req, res) => {
-    res.sendFile("index.html", { root: path.join(__dirname, "../../../public") });
+    res.sendFile("index.html", { root: path.join(__dirname, "../../../dist") });
 });
 
 pageRouter.get("/modelA", async (req, res) => {
-    res.sendFile("index.html", { root: path.join(__dirname, "../../../public") });
+    res.sendFile("index.html", { root: path.join(__dirname, "../../../dist") });
 });
 
 pageRouter.get("/modelB", async (req, res) => {
-    res.sendFile("index.html", { root: path.join(__dirname, "../../../public") });
+    res.sendFile("index.html", { root: path.join(__dirname, "../../../dist") });
 });
 
 pageRouter.get("/modelC", async (req, res) => {
-    res.sendFile("index.html", { root: path.join(__dirname, "../../../public") });
+    res.sendFile("index.html", { root: path.join(__dirname, "../../../dist") });
 });
 
 pageRouter.get("/modelVersion", async (req, res) => {
-    res.sendFile("index.html", { root: path.join(__dirname, "../../../public") });
+    res.sendFile("index.html", { root: path.join(__dirname, "../../../dist") });
 });
 
 pageRouter.get("/resource", async (req, res) => {
-    res.sendFile("index.html", { root: path.join(__dirname, "../../../public") });
+    res.sendFile("index.html", { root: path.join(__dirname, "../../../dist") });
 });
 
 pageRouter.get("/account", async (req, res) => {
-    res.sendFile("index.html", { root: path.join(__dirname, "../../../public") });
+    res.sendFile("index.html", { root: path.join(__dirname, "../../../dist") });
 });
 
 module.exports = pageRouter;
