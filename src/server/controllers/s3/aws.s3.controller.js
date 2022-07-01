c7c33dbf4b9bcbdb4a72d0b38aaab1fee2b3e3e0
Enchi <enchi0215@gmail.com>
2022-07-01 23:26:51 +0800

    typesetting

--- src/server/controllers/s3/aws.s3.controller.js
+++ src/server/controllers/s3/aws.s3.controller.js
@@ -4,8 +4,8 @@ const aws = require('aws-sdk');
 
 //configure the aws environment
 aws.config.update({
-    accessKeyId:process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 //initialize s3
@@ -16,23 +16,22 @@ const constantParams = {
     Bucket: process.env.AWS_BUCKET_NAME,
 }
 
-//upload file to s3 bucker
-exports.uploadToS3 = (file, next) =>{
+//upload file to s3 bucket
+exports.uploadToS3 = (file, next) => {
     const fileStream = fs.createReadStream(file.tempFilePath);
-
     const params = {
         ...constantParams,
         Body: fileStream,
         Key: file.name
     };
-    s3.upload(params, (error, data)=>{
+    s3.upload(params, (error, data) => {
         console.log(error, data);
         next(error, data);
     });
 };
 
 //download file from s3 bucket
-exports.getFileFromS3 = (folder, key) =>{
+exports.getFileFromS3 = (folder, key) => {
     const downloadParams = {
         Key: folder + '/' + key,
        ...constantParams
@@ -40,14 +39,13 @@ exports.getFileFromS3 = (folder, key) =>{
     return s3.getObject(downloadParams).createReadStream();
 };
 
-//delete file from s3 bucker
-exports.deleteFileFromS3 = (folder, key, next) =>{
+//delete file from s3 bucket
+exports.deleteFileFromS3 = (folder, key, next) => {
     const deleteParams = {
         Key: folder + '/' + key,
         ...constantParams
     };
-    s3.deleteObject(deleteParams, (error, data)=>{
-
+    s3.deleteObject(deleteParams, (error, data) => {
         next(error, data);
     });
 };
--- src/server/routes/api/awsS3Router.js
+++ src/server/routes/api/awsS3Router.js
@@ -2,18 +2,18 @@ const express = require('express');
 const awsS3Router = express.Router();
 const s3 = require("../../controllers/s3/aws.s3.controller");
 
-awsS3Router.post("/upload", (req, res)=>{
+awsS3Router.post("/upload", (req, res) => {
     const file = req.files.file;
-    s3.uploadToS3(file, (error, data)=>{
+    s3.uploadToS3(file, (error, data) => {
         console.log("commit")
-        if(error){
+        if (error) {
             return res.send({error:"Something went wrong."});
         }
         return res.send({message:"File uploaded successfully"});
     });
 });
 
-awsS3Router.get("/getFile/:folder/:files", async (req, res)=>{
+awsS3Router.get("/getFile/:folder/:files", async(req, res) => {
     const getFolder = req.params.folder;
     const getFile = req.params.files;
     try {
@@ -25,11 +25,11 @@ awsS3Router.get("/getFile/:folder/:files", async (req, res)=>{
     }
 });
 
-awsS3Router.delete("/deleteFile/:folder/:files", (req, res)=>{
+awsS3Router.delete("/deleteFile/:folder/:files", (req, res) => {
     const getFolder = req.params.folder;
     const getFile = req.params.files;
-    s3.deleteFileFromS3(getFolder, getFile, (error, data)=>{
-        if(error){
+    s3.deleteFileFromS3(getFolder, getFile, (error, data) => {
+        if (error) {
             return res.send({error:"Can not delete file, Please try again later"});
         }
         return res.send({message:"File has been deleted successfully"});
