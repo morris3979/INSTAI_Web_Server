e6d5910d0ebe6ed17c5698477014a13766313f83
Enchi <enchi0215@gmail.com>
2022-08-08 01:01:33 +0800

    update

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,12 +7,12 @@ sudo chmod -R 777 /home/ec2-user/lab321-app
 cd /home/ec2-user/lab321-app
 
 #add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 npm install
 
 #start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null & 
+node app.js > app.out.log 2> app.err.log < /dev/null &
