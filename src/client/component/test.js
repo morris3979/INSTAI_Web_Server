2eacbd7bdbe71b2e9e318dbf371f6c0f90c8cb8d
Enchi <enchi0215@gmail.com>
2022-10-06 13:53:51 +0800

    update test page & hostTable

--- src/client/component/test.js
+++ src/client/component/test.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import { Table, Button, Typography, Collapse } from 'antd'
 import { DownloadOutlined } from '@ant-design/icons'
 import ReactPlayer from 'react-player/lazy'
@@ -9,8 +9,19 @@ const { Panel } = Collapse
 
 const data = [
   {
+    key: 1,
     videoURL: 'https://d20cmf4o2f77jz.cloudfront.net/video/00000000674a3751_1_20220728192323_camera1_front_body.mp4',
     description: '00000000674a3751_1_20220728192323_camera1_front_body'
+  },
+  {
+    key: 2,
+    videoURL: 'https://d20cmf4o2f77jz.cloudfront.net/video/00000000674a3751_1_20220728192058_camera1_front_body.mp4',
+    description: '00000000674a3751_1_20220728192058_camera1_front_body'
+  },
+  {
+    key: 3,
+    videoURL: 'https://d20cmf4o2f77jz.cloudfront.net/video/00000000674a3751_1_20220808202505_camera1_front_body.mp4',
+    description: '00000000674a3751_1_20220808202505_camera1_front_body'
   }
 ]
 
@@ -44,14 +55,55 @@ const download = () => {
 }
 
 const Test = () => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const start = () => {
+    setLoading(true); // ajax request after empty completing
+
+    setTimeout(() => {
+      setSelectedRowKeys([]);
+      setLoading(false);
+    }, 1000);
+  };
+
+  const onSelectChange = (newSelectedRowKeys) => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+
+  const hasSelected = selectedRowKeys.length > 0;
+
   return (
     <Fragment>
       <Collapse>
-        <Panel header='0000000039aed1d2'>
+        <Panel header='0000000039aed1d2 (RasPi_01)'>
           <Collapse>
-            <Panel header='0x7680'>
+            <Panel header='0x7680 (PAG_01)'>
+              <div
+                style={{
+                  marginBottom: 16,
+                }}
+              >
+                <Button type="primary" onClick={start} disabled={!hasSelected} loading={loading}>
+                  send
+                </Button>
+                <span
+                  style={{
+                    marginLeft: 8,
+                  }}
+                >
+                  {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
+                </span>
+              </div>
               <Table
                 dataSource={data}
+                rowSelection={rowSelection}
                 pagination={{ position: ['bottomCenter'] }}
               >
                 <Column
@@ -71,11 +123,28 @@ const Test = () => {
         </Panel>
       </Collapse>
       <Collapse>
-        <Panel header='0000000012aed3d4'>
+        <Panel header='0000000012aed3d4 (RasPi_02)'>
           <Collapse>
-            <Panel header='0x7680'>
+            <Panel header='0x7680 (PAG_02)'>
+              <div
+                style={{
+                  marginBottom: 16,
+                }}
+              >
+                <Button type="primary" onClick={start} disabled={!hasSelected} loading={loading}>
+                  send
+                </Button>
+                <span
+                  style={{
+                    marginLeft: 8,
+                  }}
+                >
+                  {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
+                </span>
+              </div>
               <Table
                 dataSource={data}
+                rowSelection={rowSelection}
                 pagination={{ position: ['bottomCenter'] }}
               >
                 <Column
