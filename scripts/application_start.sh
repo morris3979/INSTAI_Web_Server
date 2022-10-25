2616cd1eafbbb4131b858f2ddb300341f6cbef41
Enchi <enchi0215@gmail.com>
2022-10-25 20:40:24 +0800

    add an automation script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-#give permission for everything in the lab321-app directory
-sudo chmod -R 777 /home/ec2-user/lab321-app
-
-#navigate into our working directory where we have all our github files
-cd /home/ec2-user/lab321-app
-
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-#install node modules
-npm install
-
-#start our node app in the background
-npm start > app.out.log 2> app.err.log < /dev/null &
+# Stop all servers and start the server as a daemon
+forever stopall
+pkill -f nodejs
+npm start
