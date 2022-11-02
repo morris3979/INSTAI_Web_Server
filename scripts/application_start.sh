e6a75eb684de5dc85b015baa518e96dac1143aac
Enchi <enchi0215@gmail.com>
2022-11-02 12:52:44 +0800

    fix auto script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Stop all servers and start the server as a daemon
-sudo forever stopall
-sudo pkill -f nodejs
+# sudo forever stopall
+sudo pkill -f node
 sudo pm2 stop all
 sudo pm2 delete all
 
