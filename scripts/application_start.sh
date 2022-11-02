e351a0ac2e9c282b48b72a6bfd782a12693cc116
Enchi <enchi0215@gmail.com>
2022-11-03 00:33:07 +0800

    update

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -15,4 +15,4 @@ export NVM_DIR="$HOME/.nvm"
 npm install
 
 #start our node app in the background
-sudo pm2 start npm --name instai -- start > app.out.log 2> app.err.log < /dev/null &
+npm start > app.out.log 2> app.err.log < /dev/null &
