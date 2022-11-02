e88fe3df66a158d4d62ec72f58438695123306b1
Enchi <enchi0215@gmail.com>
2022-11-02 21:23:20 +0800

    update

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-# Stop all servers and start the server as a daemon
-# sudo forever stopall
-npm uninstall -g pm2
+#Stopping existing node servers
 echo "Stopping any existing node servers"
 pkill node
