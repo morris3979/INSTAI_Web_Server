5b810dba867e01a90dcf670c2491691821944b64
Enchi <enchi0215@gmail.com>
2022-11-03 20:05:23 +0800

    test

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,12 +6,12 @@ sudo chmod -R 777 /home/ec2-user/my_project
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/my_project
 
-#add npm and node to path
+# add npm and node to path
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-#install node modules
+# install node modules
 npm install --legacy-peer-deps
 
 # stop and clean
