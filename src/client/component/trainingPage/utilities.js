27ef077d2e0ee80572cb519d6ec8d5f33ce58b38
Enchi <enchi0215@gmail.com>
2022-12-10 14:00:44 +0800

    update

--- src/client/component/trainingPage/utilities.js
+++ src/client/component/trainingPage/utilities.js
@@ -9,12 +9,12 @@ export const drawRect = (detections, ctx) =>{
       const text = ' '+detectObj+'  '+score;
 
       // Set styling
-      ctx.strokeStyle = '#00FFFF';
+      ctx.strokeStyle = '#00FF00';
       const font = '18px sans-serif';
       ctx.font = font;
       ctx.lineWidth = 4;
       ctx.textBaseline = "top";
-      ctx.fillStyle = '#00FFFF';
+      ctx.fillStyle = '#00FF00';
       const textWidth = ctx.measureText(text).width;
       const textHeight = parseInt(font, 10); // base 10
       ctx.fillRect(x, y, textWidth + 6, textHeight + 6);
