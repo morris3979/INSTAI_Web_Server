9fd72adeb50df9ebc6c6ef0d18e1174d9cb1713e
Enchi <enchi0215@gmail.com>
2023-01-09 12:14:11 +0800

    add pageRoute

--- src/server/routes/api/page.routes.js
+++ src/server/routes/api/page.routes.js
@@ -6,6 +6,14 @@ pageRouter.get("/", async (req, res) => {
     res.sendFile("index.html", { root: path.join(__dirname, "../../../../dist") });
 });
 
+pageRouter.get("/Register", async (req, res) => {
+    res.sendFile("index.html", { root: path.join(__dirname, "../../../../dist") });
+});
+
+pageRouter.get("/Organization", async (req, res) => {
+    res.sendFile("index.html", { root: path.join(__dirname, "../../../../dist") });
+});
+
 pageRouter.get("/Home", async (req, res) => {
     res.sendFile("index.html", { root: path.join(__dirname, "../../../../dist") });
 });
